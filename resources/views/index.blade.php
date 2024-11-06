821da018c5c0107099b20ce9061e7be722901ae5
Tomáš Zaharowski <tomas.zaharowski@o2.cz>
2024-11-06 20:47:05 +0100

    init commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,6 @@
+<x-layouts.app>
+
+    Jsem tady Hello world
+
+
+</x-layouts.app>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [DashboardController::class, 'index']); 
+
